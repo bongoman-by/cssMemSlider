dd572f4a907bbe7f86ce5ad36a6d676149a87fe0
bongoman-by <bongoman@tut.by>
2022-05-12 09:35:02 +0300

    refactor(cssMemSlider): delete pseudo element ::before (THU, May 12, 2022 11:10:30 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -6,16 +6,18 @@ const slides = carouselElem.querySelectorAll(".carousel-item");
 const slidesField = carouselElem.querySelector(".carousel-slides");
 slidesField.style.width = 100 * slides.length + "%";
 const dots = carouselElem.querySelectorAll(".carousel-indicators li");
-const description = carouselElem.querySelector("h1");
+const radios = carouselElem.querySelectorAll(".radio");
+const h2Elem = carouselElem.querySelector("h2");
 let slideIndex = 0;
 
-function Carousel() {
+function carousel() {
   slides.forEach((slide) => {
     slide.style.width = width;
   });
 
   dots.forEach((dot) => {
     dot.addEventListener("click", getSlide);
+
     dot.addEventListener("mousedown", (e) => {
       document.body.style.setProperty(
         "--xy",
@@ -23,40 +25,128 @@ function Carousel() {
       );
       document.body.classList.add("mouse-press");
     });
+
+    dot.addEventListener("touchstart", (e) => {
+      document.body.style.setProperty(
+        "--xy",
+        e.changedTouches[0].clientX + "px " + e.changedTouches[0].clientY + "px"
+      );
+      document.body.classList.add("mouse-press");
+      document.body.classList.remove("mouse-move");
+    });
+
     dot.addEventListener("mouseup", (e) =>
       document.body.classList.remove("mouse-press")
     );
+
+    dot.addEventListener("touchend", (e) => {
+      document.body.classList.remove("mouse-press");
+      if (e.target.children[0]) {
+        e.target.children[0].classList.remove("hover");
+      }
+    });
+
+    dot.addEventListener("touchcancel", (e) => {
+      document.body.classList.remove("mouse-press");
+      if (e.target.children[0]) {
+        e.target.children[0].classList.remove("hover");
+      }
+    });
+
+    dot.addEventListener("mousemove", (e) => {
+      if (e.target.children[0]) {
+        e.target.children[0].classList.add("hover");
+      }
+    });
+
+    dot.addEventListener("touchmove", (e) => {
+      if (e.target.children[0]) {
+        e.target.children[0].classList.add("hover");
+      }
+    });
+
+    dot.addEventListener("mouseout", (e) => {
+      if (e.target.children[0]) {
+        e.target.children[0].classList.remove("hover");
+      }
+    });
   });
 
-  description.addEventListener("mousemove", (e) => {
+  h2Elem.addEventListener("mousemove", (e) => {
     document.body.style.setProperty(
       "--xy",
       e.clientX + "px " + e.clientY + "px"
     );
+    document.body.classList.remove("mouse-move");
     document.body.classList.add("mouse-press");
   });
-  description.addEventListener("mouseout", (e) =>
+
+  h2Elem.addEventListener("touchmove", (e) => {
+    document.body.style.setProperty(
+      "--xy",
+      e.changedTouches[0].clientX + "px " + e.changedTouches[0].clientY + "px"
+    );
+    document.body.classList.remove("mouse-move");
+    document.body.classList.add("mouse-press");
+  });
+
+  h2Elem.addEventListener("mouseout", (e) =>
+    document.body.classList.remove("mouse-press")
+  );
+
+  h2Elem.addEventListener("touchcancel", (e) =>
+    document.body.classList.remove("mouse-press")
+  );
+
+  h2Elem.addEventListener("touchend", (e) =>
     document.body.classList.remove("mouse-press")
   );
 
   carouselElem.addEventListener("mousemove", (e) => {
-    if (e.target === description) {
+    if (e.target.nodeName === "SPAN") {
       return;
     }
+
     document.body.style.setProperty(
       "--xy",
       e.clientX + "px " + e.clientY + "px"
     );
     document.body.classList.add("mouse-move");
   });
+
+  carouselElem.addEventListener("touchmove", (e) => {
+    if (e.target.nodeName === "SPAN") {
+      return;
+    }
+    document.body.style.setProperty(
+      "--xy",
+      e.changedTouches[0].clientX + "px " + e.changedTouches[0].clientY + "px"
+    );
+    document.body.classList.add("mouse-move");
+  });
+
   carouselElem.addEventListener("mouseout", (e) =>
     document.body.classList.remove("mouse-move")
   );
+
+  carouselElem.addEventListener("touchcancel", (e) =>
+    document.body.classList.remove("mouse-move")
+  );
+
+  carouselElem.addEventListener("touchend", (e) =>
+    document.body.classList.remove("mouse-move")
+  );
+
   carouselElem.addEventListener("mousedown", (e) =>
     document.body.classList.remove("mouse-move")
   );
+
+  carouselElem.addEventListener("touchstart", (e) =>
+    document.body.classList.remove("mouse-move")
+  );
+
   carouselElem.addEventListener("mouseup", (e) => {
-    if (e.target === description) {
+    if (e.target.nodeName === "SPAN") {
       return;
     }
     document.body.classList.add("mouse-move");
@@ -64,19 +154,33 @@ function Carousel() {
 }
 
 function getSlide(e) {
-  const slideTo = e.target.getAttribute("data-slide-to");
-  description.textContent = e.target.getAttribute("data-description");
+  let slideTo = "";
+  if (e.target.children[0]) {
+    slideTo = e.target.children[0].getAttribute("data-slide-to");
+    h2Elem.innerHTML = `<span>${e.target.children[0].getAttribute(
+      "data-description"
+    )}</span>`;
+  } else {
+    slideTo = e.target.getAttribute("data-slide-to");
+    h2Elem.innerHTML = `<span>${e.target.getAttribute(
+      "data-description"
+    )}</span>`;
+  }
 
   slideIndex = +slideTo;
   offset = +width.replace(/\D/g, "") * slideTo;
 
   slidesField.style.transform = `translateX(-${offset}px)`;
-  dots.forEach((dot) => dot.classList.remove("active"));
-  dots[slideIndex].classList.add("active");
+  radios.forEach((radio) => radio.classList.remove("active"));
+  if (e.target.children[0]) {
+    e.target.children[0].classList.add("active");
+  } else {
+    e.target.classList.add("active");
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  Carousel();
+  carousel();
 });
 
 window.addEventListener("resize", () => {
@@ -85,5 +189,5 @@ window.addEventListener("resize", () => {
   ).width;
   offset = +width.replace(/\D/g, "") * slideIndex;
   slidesField.style.transform = `translateX(-${offset}px)`;
-  Carousel();
+  carousel();
 });
