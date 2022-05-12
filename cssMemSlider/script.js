b801e2b8a6b368541eb8ac0e387d03b9ef7e3c8c
bongoman-by <bongoman@tut.by>
2022-05-12 09:35:02 +0300

    refactor(cssMemSlider): delete pseudo element ::before

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -6,7 +6,9 @@ const slides = carouselElem.querySelectorAll(".carousel-item");
 const slidesField = carouselElem.querySelector(".carousel-slides");
 slidesField.style.width = 100 * slides.length + "%";
 const dots = carouselElem.querySelectorAll(".carousel-indicators li");
-const description = carouselElem.querySelector("h1");
+const radios = carouselElem.querySelectorAll(".radio");
+const h2Elem = carouselElem.querySelector("h2");
+const spanElem = carouselElem.querySelector("span");
 let slideIndex = 0;
 
 function Carousel() {
@@ -26,21 +28,34 @@ function Carousel() {
     dot.addEventListener("mouseup", (e) =>
       document.body.classList.remove("mouse-press")
     );
+    dot.addEventListener("mouseup", (e) =>
+      document.body.classList.remove("mouse-press")
+    );
+    dot.addEventListener("mousemove", (e) => {
+      if (e.target.children[0]) {
+        e.target.children[0].classList.add("hover");
+      }
+    });
+    dot.addEventListener("mouseout", (e) => {
+      if (e.target.children[0]) {
+        e.target.children[0].classList.remove("hover");
+      }
+    });
   });
 
-  description.addEventListener("mousemove", (e) => {
+  h2Elem.addEventListener("mousemove", (e) => {
     document.body.style.setProperty(
       "--xy",
       e.clientX + "px " + e.clientY + "px"
     );
     document.body.classList.add("mouse-press");
   });
-  description.addEventListener("mouseout", (e) =>
+  h2Elem.addEventListener("mouseout", (e) =>
     document.body.classList.remove("mouse-press")
   );
 
   carouselElem.addEventListener("mousemove", (e) => {
-    if (e.target === description) {
+    if (e.target === h2Elem) {
       return;
     }
     document.body.style.setProperty(
@@ -56,7 +71,7 @@ function Carousel() {
     document.body.classList.remove("mouse-move")
   );
   carouselElem.addEventListener("mouseup", (e) => {
-    if (e.target === description) {
+    if (e.target === h2Elem) {
       return;
     }
     document.body.classList.add("mouse-move");
@@ -64,15 +79,29 @@ function Carousel() {
 }
 
 function getSlide(e) {
-  const slideTo = e.target.getAttribute("data-slide-to");
-  description.textContent = e.target.getAttribute("data-description");
+  let slideTo = "";
+  if (e.target.children[0]) {
+    slideTo = e.target.children[0].getAttribute("data-slide-to");
+    h2Elem.innerHTML = `<span>${e.target.children[0].getAttribute(
+      "data-description"
+    )}</span>`;
+  } else {
+    slideTo = e.target.getAttribute("data-slide-to");
+    h2Elem.innerHTML = `<span>${e.target.getAttribute(
+      "data-description"
+    )}</span>`;
+  }
 
   slideIndex = +slideTo;
   offset = +width.replace(/\D/g, "") * slideTo;
 
   slidesField.style.transform = `translateX(-${offset}px)`;
-  dots.forEach((dot) => dot.classList.remove("active"));
-  dots[slideIndex].classList.add("active");
+  radios.forEach((radio) => radio.classList.remove("active"));
+  if (e.target.children[0]) {
+    e.target.children[0].classList.add("active");
+  } else {
+    e.target.classList.add("active");
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
